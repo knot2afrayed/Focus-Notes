f7d185b831050bf50cfe193ed2a0336cd4db2c68
Shawn Haynes <shawnhaynes@shawns-imac.local>
2017-01-31 21:56:55 -0500

    Initial Commit

---
+++ Focus Notes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Focus Notes
+//
+//  Created by Shawn Haynes on 1/31/17.
+//  Copyright © 2017 Shawn Haynes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
