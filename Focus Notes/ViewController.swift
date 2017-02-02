841ad7d61327038073dd3c2d563103e319a01de2
Shawn Haynes <shawnhaynes@shawns-imac.local>
2017-02-02 07:31:31 -0500

    initial commit

--- Focus Notes/ViewController.swift
+++ Focus Notes/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var numberA: UITextField!
+    @IBOutlet var numberB: UITextField!
+    
+    @IBOutlet var AnswerText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func gcd(_ a: Int, _ b: Int) -> Int {
+        let r = a % b
+        if r != 0 {
+            return gcd(b, r)
+        } else {
+            return b
+        }
+    }
+    @IBAction func calcGCF(_ sender: Any) {
+        if let tryA = Int(numberA.text!), let tryB = Int(numberB.text!){
+       
+        let answer = gcd(tryA, tryB)
+            AnswerText.text = "\(answer)"
+        }
+    }
+    
 }
 
